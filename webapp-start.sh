6c96f4c2e13031307a2ff34b0be31e47b42f952a
rzii <mangekyo6468@gmail.com>
2020-10-18 12:38:32 +0700

    add script #2

---
+++ webapp-start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+az webapp start --name pilketos --resource-group pilketos
+az mysql server start --name db-pilketos --resource-group tes-pilketos
+
---
+++ webapp-stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+az webapp stop --name pilketos --resource-group pilketos
+az mysql server stop --name db-pilketos --resource-group tes-pilketos
+
